69ce3fc50d5b12da57339ee0dc39f61b8eecf876
Coceki <43731752+Coceki@users.noreply.github.com>
2020-06-13 00:03:09 +0800

    基本功能已完全移植

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,30 @@
     <div class="titlediv">
       <p class="title" :class="{dark_text:$root.dark}" >{{$t("title")}}</p>
     </div>
-    <!-- <div class="topdiv">
-      <div class="card">
-        <p class="cardtext">{{$t("info")}}
+    <div class="topdiv topdivO">
+      <div class="card cardM" :class="{dark:$root.dark}">
+        <p class="cardtext" :class="{dark_text:$root.dark}"><svg style="width:42px;height:42px;padding:3px" viewBox="0 0 24 24">
+    <path fill="red" d="M14.53 1.45L13.45 2.53L15.05 4.13C15.27 4.38 15.38 4.67 15.38 5S15.27 5.64 15.05 5.86L11.5 9.47L12.5 10.55L16.13 6.94C16.66 6.35 16.92 5.7 16.92 5C16.92 4.3 16.66 3.64 16.13 3.05L14.53 1.45M10.55 3.47L9.47 4.55L10.08 5.11C10.3 5.33 10.41 5.63 10.41 6S10.3 6.67 10.08 6.89L9.47 7.45L10.55 8.53L11.11 7.92C11.64 7.33 11.91 6.69 11.91 6C11.91 5.28 11.64 4.63 11.11 4.03L10.55 3.47M21 5.06C20.31 5.06 19.67 5.33 19.08 5.86L13.45 11.5L14.53 12.5L20.11 6.94C20.36 6.69 20.66 6.56 21 6.56S21.64 6.69 21.89 6.94L22.5 7.55L23.53 6.47L22.97 5.86C22.38 5.33 21.72 5.06 21 5.06M7 8L2 22L16 17L7 8M19 11.06C18.3 11.06 17.66 11.33 17.06 11.86L15.47 13.45L16.55 14.53L18.14 12.94C18.39 12.69 18.67 12.56 19 12.56C19.33 12.56 19.63 12.69 19.88 12.94L21.5 14.53L22.55 13.5L20.95 11.86C20.36 11.33 19.7 11.06 19 11.06Z" />
+</svg>{{$t("info")}}
         </p>
-        <div class="infotext">{{$t("betainfo")}}</div>
+        <div class="infotext" :class="{dark_infotext:$root.dark}">{{$t("betainfo")}}</div>
+        <button
+            class="btn ripple"
+            :class="{dark_btn:$root.dark}"
+            @click="playSpecial()"
+          ><div>Co↘ce↗ki↘お兄ちゃん</div></button>
       </div>
-      <div class="card">
-        <p class="cardtext">{{$t("info")}}
+      <div class="card cardM" :class="{dark:$root.dark}">
+        <p class="cardtext" :class="{dark_text:$root.dark}"><svg style="width:42px;height:42px" viewBox="0 0 24 24">
+    <path fill="#728bf3" d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+</svg>{{$t("helptitle")}}
         </p>
-        <div class="infotext">{{$t("betainfo")}}</div>
+        <div class="infotext" :class="{dark_infotext:$root.dark}">{{$t("helpcontent")}}<br>{{$t("additionalhelp")}}</div>
       </div>
-      <div class="card">
-        <p class="cardtext">{{$t("info")}}
-        </p>
-        <div class="infotext">{{$t("betainfo")}}</div>
-      </div>
-      <div class="card">
-        <p class="cardtext">{{$t("info")}}
-        </p>
-      </div>
+     
     </div>
-    <hr color="#EEE" width="95%" /> -->
+    <hr class="line" v-if="$root.dark" color=#333 width=95% />
+    <hr class="line" v-else color=#EEE width=95% />
     <div class="topdiv">
       <div class="card" :class="{dark:$root.dark}" v-for="(group,index) in voices" :key="index">
         <p class="cardtext" :class="{dark_text:$root.dark}" v-if="$i18n.locale=='zhHans'">{{group.translation.Chinese}}</p>
@@ -171,6 +173,7 @@ body {
 }
 .card {
   padding: 20px;
+  padding-bottom: 40px;
   margin:15px;
   overflow:visible;
   break-inside: avoid;
@@ -181,6 +184,9 @@ body {
   border-radius: 16px;
   transition: 0.5s;
 }
+.cardM{
+  max-width: 100% !important;
+}
 .topdiv {
   overflow:visible;
   column-gap: 0px;
@@ -189,6 +195,9 @@ body {
   box-sizing: border-box;
   align-items: center;
   width: 100%;
+}
+.topdivO{
+  column-count: 1;
 }
 .cardtext {
   overflow: visible;
@@ -298,6 +307,9 @@ body {
   font-family: Helvetica;
   font-size: 16px;
   letter-spacing: 0.02em;
+}
+.dark_infotext{
+  color: #e9e9e9;
 }
 .overflowC{
   overflow: visible;
