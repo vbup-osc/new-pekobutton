e086a05b6ffb8e1eca4af0f2e4c752a9a40455ef
Coceki <43731752+Coceki@users.noreply.github.com>
2020-08-10 13:45:34 +0800

    Fix translation Use holotools api instead of unstable old api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -182,6 +182,99 @@
           <div>Co↘ce↗ki↘お兄ちゃん</div>
         </s-btn>
       </div>
+      <!-- 显示直播订阅信息 -->
+      <div class="card cardM" :class="{dark:$root.dark}">
+        <p class="cardtext" :class="{dark_text:$root.dark}">
+          <svg style="width:42px;height:42px;transform: translate3d(0,30%,0);" viewBox="0 0 24 24">
+            <path
+              fill="red"
+              d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z"
+            />
+          </svg>
+          {{$t("youtubesubs")}}
+        </p>
+        <div  class="subnum" :class="{dark_infotext:$root.dark}">{{youtubeData.subscriber_count}}</div>
+        <div
+          :class="{dark_infotext:$root.dark}"
+          style="
+              letter-spacing: 0.02em;margin-top:40px;margin-left:20px"
+        >{{$t("streaming")}}</div>
+        <!-- 正在直播列表 -->
+        <div class="liveinfo">
+          <div
+            v-for="(streaming,index3) in live_data"
+            :key="index3"
+          >
+          <div class="card cardM"
+            :class="{dark:$root.dark}"
+            style="padding: 5px !important;
+                  padding-bottom: 20px !important;
+                  margin: 10px !important;"
+            v-if="streaming.status === 'live'">
+            <!-- <img width="200px" class="liveimg" :src="upcoming.snippet.thumbnails.medium.url"/> -->
+            <p  class="livetext" :class="{dark_text:$root.dark}">
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path
+                  fill="#728bf3"
+                  d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12S17.5 2 12 2M17 13H11V7H12.5V11.5H17V13Z"
+                />
+              </svg>
+              {{new Date(streaming.live_start).toLocaleString()}}{{streaming.title}}
+            </p>
+            <p :class="{dark_text:$root.dark}">{{streaming.description}}</p>
+            <br />
+            <button
+              class="goto"
+              @click="See('https://www.youtube.com/watch?v='+streaming.yt_video_key)"
+            >
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </button>
+          </div>
+          </div>
+        </div>
+        <div
+          :class="{dark_infotext:$root.dark}"
+          style="
+              letter-spacing: 0.02em;margin-top:40px;margin-left:20px"
+        >{{$t("upcoming")}}</div>
+        <!-- 即将直播的列表 -->
+        <div class="liveinfo">
+          <div
+            v-for="(upcoming,index2) in live_data"
+            :key="index2"
+          >
+          <div class="card cardM"
+            :class="{dark:$root.dark}"
+            style="padding: 5px !important;
+                  padding-bottom: 20px !important;
+                  margin: 10px !important;"
+            v-if="upcoming.status === 'upcoming'">
+            <!-- <img width="200px" class="liveimg" :src="upcoming.snippet.thumbnails.medium.url"/> -->
+            <p class="livetext" :class="{dark_text:$root.dark}">
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path
+                  fill="#728bf3"
+                  d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12S17.5 2 12 2M17 13H11V7H12.5V11.5H17V13Z"
+                />
+              </svg>
+              {{new Date(upcoming.live_schedule).toLocaleString()}}{{upcoming.title}}
+            </p>
+            <p :class="{dark_text:$root.dark}">{{upcoming.description}}</p>
+            <br />
+            <button
+              class="goto"
+              @click="See('https://www.youtube.com/watch?v='+upcoming.yt_video_key)"
+            >
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </button>
+          </div>
+          </div>
+        </div>
+      </div>
       <div class="card cardM" :class="{dark:$root.dark}">
         <p class="cardtext" :class="{dark_text:$root.dark}">
           <svg style="width:42px;height:42px;transform: translate3d(0,30%,0);" viewBox="0 0 24 24">
@@ -223,95 +316,7 @@
           <a style="margin-left:30px">@usadapekora</a>
         </s-btn>
       </div>
-      <!-- 显示直播订阅信息 -->
-      <div class="card cardM" :class="{dark:$root.dark}">
-        <p class="cardtext" :class="{dark_text:$root.dark}">
-          <svg style="width:42px;height:42px;transform: translate3d(0,30%,0);" viewBox="0 0 24 24">
-            <path
-              fill="red"
-              d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z"
-            />
-          </svg>
-          {{$t("youtubesubs")}}
-        </p>
-        <div class="subnum" :class="{dark_infotext:$root.dark}">{{channel_subs}}</div>
-        <div
-          :class="{dark_infotext:$root.dark}"
-          style="
-              letter-spacing: 0.02em;margin-top:40px;margin-left:20px"
-        >{{$t("streaming")}}</div>
-        <!-- 正在直播列表 -->
-        <div class="liveinfo">
-          <div
-            class="card cardM"
-            :class="{dark:$root.dark}"
-            style="padding: 5px !important;
-                  padding-bottom: 20px !important;
-                  margin: 10px !important;"
-            v-for="(streaming,index3) in channel_streaming"
-            :key="index3"
-          >
-            <!-- <img width="200px" class="liveimg" :src="upcoming.snippet.thumbnails.medium.url"/> -->
-            <p class="livetext" :class="{dark_text:$root.dark}">
-              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path
-                  fill="#728bf3"
-                  d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12S17.5 2 12 2M17 13H11V7H12.5V11.5H17V13Z"
-                />
-              </svg>
-              {{streaming.snippet.title}}
-            </p>
-            <p :class="{dark_text:$root.dark}">{{streaming.snippet.description}}</p>
-            <br />
-            <button
-              class="goto"
-              @click="See('https://www.youtube.com/watch?v='+streaming.id.videoId)"
-            >
-              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div
-          :class="{dark_infotext:$root.dark}"
-          style="
-              letter-spacing: 0.02em;margin-top:40px;margin-left:20px"
-        >{{$t("upcoming")}}</div>
-        <!-- 即将直播的列表 -->
-        <div class="liveinfo">
-          <div
-            class="card cardM"
-            :class="{dark:$root.dark}"
-            style="padding: 5px !important;
-                  padding-bottom: 20px !important;
-                  margin: 10px !important;"
-            v-for="(upcoming,index2) in channel_upcoming"
-            :key="index2"
-          >
-            <!-- <img width="200px" class="liveimg" :src="upcoming.snippet.thumbnails.medium.url"/> -->
-            <p class="livetext" :class="{dark_text:$root.dark}">
-              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path
-                  fill="#728bf3"
-                  d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12S17.5 2 12 2M17 13H11V7H12.5V11.5H17V13Z"
-                />
-              </svg>
-              {{upcoming.snippet.title}}
-            </p>
-            <p :class="{dark_text:$root.dark}">{{upcoming.snippet.description}}</p>
-            <br />
-            <button
-              class="goto"
-              @click="See('https://www.youtube.com/watch?v='+upcoming.id.videoId)"
-            >
-              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="white" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
+      
     </div>
     <hr class="line" v-if="$root.dark" color="#333" width="95%" />
     <hr class="line" v-else color="#EEE" width="95%" />
@@ -390,48 +395,42 @@ export default {
     repeatmode: false,
     arrysize: 0,
     volume: 100,
-    channel_info: null,
-    channel_subs: null,
-    channel_upcoming: null,
-    channel_streaming: null
+    live_data: {},
+    live_data_loading:true,
+    youtubeData:{channels: null}
   }),
   created() {
     //window.console.log(this.voices); //装载语音包path
     const axios = require("axios");
-    axios
-      .get("/api/php/youtubesub.php") //获取油管订阅信息
-      .then(response => {
-        //window.console.log(response.data);
-        this.channel_info = response.data;
-        this.channel_subs = this.channel_info.items[0].statistics.subscriberCount;
-        //window.console.log(this.channel_subs);
+    axios.get('https://api.holotools.app/v1/channels/youtube/UC1DCedRgGHBdm81E1llLhOQ')
+        .then(response => {
+        this.youtubeData = response.data
+        })
+  },
+  mounted(){
+    const axios = require("axios");
+    axios.get('https://api.holotools.app/v1/live')
+        .then(response => { 
+            let fetched = response.data;
+            let lives = [];
+            const channel_id = 'UC1DCedRgGHBdm81E1llLhOQ';
+            fetched.live.forEach(function(item){
+                if (item.channel.yt_channel_id === channel_id){
+                    item.status = 'live';
+                    lives.push(item);
+                }
+            });
+            fetched.upcoming.forEach(function(item){
+                if (item.channel.yt_channel_id === channel_id){
+                    item.status = 'upcoming';
+                    lives.push(item);
+                }
+            });
+            this.live_data = lives;
+            this.live_data_loading = false;
+            window.console.log(this.live_data);
+            window.console.log(this.youtubeData);
       })
-      .catch(function(error) {
-        console.log(error);
-      })
-      .then(function() {});
-    axios
-      .get("/api/php/upcomingstream.php") //获取开播提醒
-      .then(response => {
-        window.console.log(response.data);
-        this.channel_upcoming = response.data.items;
-        window.console.log(this.channel_upcoming);
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-      .then(function() {});
-    axios
-      .get("/api/php/streaming.php") //获取开播提醒
-      .then(response => {
-        //window.console.log(response.data);
-        this.channel_streaming = response.data.items;
-        window.console.log(this.channel_streaming);
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-      .then(function() {});
   },
   methods: {
     play(item) {
